af9d4c6404344095706959c9e363924823f0e56e
Soham <sohamroy20052005@gmail.com>
2025-06-04 23:55:21 +0530

    module rust{fn pay(){paisa}}

---
+++ src/bin/17_modules.rs
@@ -0,0 +1,17 @@
+#![allow(unused)]
+
+
+mod my {
+    pub fn print() {
+        println!("This is a function in the my module");
+    
+    }
+
+    fn f() {
+        
+    }
+}
+
+fn main() {
+    my::print();
+}
